213a1369eef329a5349cc3aaf097217a00b2c0d8
Slaven Tomac <slaven.tomac@gmail.com>
2015-05-10 22:55:13 +0200

    Added requirejs config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,11 @@
 angular
 	.module('EcmaScript6', ['ui.router'])
 	.config(function($stateProvider, $urlRouterProvider) {
+
+		require.config({
+			baseUrl: "app/scripts/modules"
+		});
+
 		//
 		// For any unmatched url, redirect to /state1
 		$urlRouterProvider.otherwise("/home");
